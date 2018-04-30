af5aef934fc2da1e6f2c8c24f88f1bae4103d818
frequem <frequem@frequem.com>
2018-04-30 13:21:12 +0200

    Added onclick example, fixed README

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 window.onload = function(){
 	alttextInit();
 };
+
+function onAlttextClick(elem, str_alt, str_orig, group){
+	console.log("clicked on:", elem);
+	console.log("Alternative String:", str_alt);
+	console.log("Original String:", str_orig);
+	console.log("Element was in Group:", group);
+}
